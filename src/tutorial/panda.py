44129f61cedf1535f9477014a30eb5df6330f246
Lachlan Clatworthy <lachlanclatworthy@proton.me>
2024-05-12 18:53:32 +1000

    Reconfigure the window to 1600x900

--- src/tutorial/panda.py
+++ src/tutorial/panda.py
@@ -1,11 +1,21 @@
 # panda.py
 
 from direct.showbase.ShowBase import ShowBase
+from panda3d.core import WindowProperties
+
+class Window:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
 class Game(ShowBase):
-    def __init__(self):
+    def __init__(self, window):
         ShowBase.__init__(self)
 
+        properties = WindowProperties()
+        properties.setSize(window.width, window.height)
+        self.win.requestProperties(properties)
+
 def run():
-    game = Game()
+    game = Game(Window(1600, 900))
     game.run()
